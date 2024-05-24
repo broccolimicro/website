8ee1e9dfa0e3fb5bdcdd49277e06f6edc85c509c
Ned Bingham <edward.bingham@broccolimicro.io>
2024-05-24 10:36:06 -0400

    adding development tools and more information

--- common.py
+++ common.py
@@ -10,15 +10,16 @@ def Post(path):
 def Navigate():
 	return Header(Id="navigate") << [
 		A(Id="title", Href="/") << [
-			Div(Id="logo") << Img(Id="logo_img", Src="logo.svg"),
-			Div(Id="name", Class="nav_button") << "Broccoli"
+			Div(Id="logo") << Img(Id="logo_img", Src="logo_simple.png"),
+			Div(Id="name", Class="nav_button") << "Broccoli",
 		],
 		Nav(Id="menu") << [
 			#A(Class="nav_button", Href="/products") << "Products",
 			A(Class="nav_button", Href="/")    << "News",
-			A(Class="nav_button", Href="/BroccoliCapabilities.pdf") << "Government",
+			A(Class="nav_button", Href="/tools.py") << "Tools",
 			A(Class="nav_button", Href="/courses.py")  << "Courses",
+			A(Class="nav_button", Href="/BroccoliCapabilities.pdf") << "Government",
 			A(Class="nav_button", Href="/about.py")  << "About",
-		]
+		],
 	]
 
